fde1e5e765bfc3ac60b9430c507ea79decc8af4d
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-12-17 10:49:31 +0330

    add test

--- src/test/java/com/bol/assignment/service/MoveRuleServiceImplTest.java
+++ src/test/java/com/bol/assignment/service/MoveRuleServiceImplTest.java
@@ -3,64 +3,86 @@ package com.bol.assignment.service;
 import com.bol.assignment.AbstractTest;
 import com.bol.assignment.exception.ServiceException;
 import com.bol.assignment.model.Board;
+import com.bol.assignment.util.GameUtil;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
 class MoveRuleServiceImplTest extends AbstractTest {
 
+    private Board board;
+
+    @BeforeEach
+    private void makeBoard() {
+        board = kalahService.newGame();
+
+    }
+
+    @AfterEach
+    private void deleteBoard() {
+        boardRepository.deleteAll();
+
+    }
 
     @Test
     void should_rearrange_game_when_pit_one_moved() {
-        Board board = kalahService.newGame();
         ruleService.replace(board, 1);
         Assertions.assertEquals(0, board.getPitsMap().get(1));
         Assertions.assertEquals(7, board.getPitsMap().get(2));
         Assertions.assertEquals(1, board.getPitsMap().get(7));
         Assertions.assertEquals(6, board.getPitsMap().get(8));
-        boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_kalah() {
-        Board board = kalahService.newGame();
         Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 0));
-        boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_game_not_found() {
         Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.empty(), 2000, 1));
-        boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_game_is_over() {
-        Board board = kalahService.newGame();
         board.setIsOver(true);
         Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 1));
-        boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_not_yours() {
-        Board board = kalahService.newGame();
         Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 8));
-        boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_empty() {
-        Board board = kalahService.newGame();
         ruleService.replace(board, 1);
         Assertions.assertEquals(0, board.getPitsMap().get(1));
         Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 1));
-        boardRepository.deleteAll();
     }
 
     @Test
-    void collectFront() {
+    void should_throw_exception_when_pit_is_invalid() {
+        board.setPitsMap(null);
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 1));
+    }
+
+    @Test
+    void should_collect_from_front_when_pit_move_to_empty_pit() {
+        ruleService.replace(board,1);
+        ruleService.setTurn(board);
+        ruleService.replace(board,2);
+        ruleService.setTurn(board);
+        ruleService.replace(board,8);
+        ruleService.setTurn(board);
+        ruleService.replace(board,1);
+        ruleService.collectFront(board);
+        Assertions.assertEquals(0,board.getPitsMap().get(2));
+        Assertions.assertEquals(0,board.getPitsMap().get(12));
+
     }
 
     @Test
@@ -68,10 +90,15 @@ class MoveRuleServiceImplTest extends AbstractTest {
     }
 
     @Test
-    void setTurn() {
+    void should_set_players_turn_when_pit_is_moved() {
+        ruleService.replace(board,1);
+        ruleService.setTurn(board);
+        Assertions.assertEquals(GameUtil.STARTED_PIT_FOR_PLAYER_ONE,board.getCurrentPlayer().getStartPitNumber());
+
+        ruleService.replace(board,2);
+        ruleService.setTurn(board);
+        Assertions.assertEquals(GameUtil.STARTED_PIT_FOR_PLAYER_TWO,board.getCurrentPlayer().getStartPitNumber());
     }
 
-    @Test
-    void validate() {
-    }
+
 }
