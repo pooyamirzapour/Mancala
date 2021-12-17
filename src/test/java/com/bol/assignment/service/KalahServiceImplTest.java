bea604858d6a45d06a0bd3bcabefbc7f0e68ad81
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-12-17 11:00:19 +0330

    add test

--- src/test/java/com/bol/assignment/service/KalahServiceImplTest.java
+++ src/test/java/com/bol/assignment/service/KalahServiceImplTest.java
@@ -1,14 +1,24 @@
 package com.bol.assignment.service;
 
 import com.bol.assignment.AbstractTest;
+import com.bol.assignment.model.Board;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KalahServiceImplTest extends AbstractTest {
 
     @Test
-    void newGame() {
+    void should_make_board_when_param_is_provided() {
+        Board board = kalahService.newGame();
+        Assertions.assertEquals(6, board.getPitsMap().get(1));
+        Assertions.assertEquals(0, board.getPitsMap().get(7));
+        Assertions.assertEquals(6, board.getPitsMap().get(8));
+        Assertions.assertEquals(0, board.getPitsMap().get(14));
+        Assertions.assertEquals(1, board.getId());
     }
 
     @Test
