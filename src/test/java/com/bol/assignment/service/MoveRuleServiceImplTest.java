4e4746ecc3a770046399e06122a787dff72db99f
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-12-17 10:12:19 +0330

    add test

--- src/test/java/com/bol/assignment/service/MoveRuleServiceImplTest.java
+++ src/test/java/com/bol/assignment/service/MoveRuleServiceImplTest.java
@@ -8,32 +8,30 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MoveRuleServiceImplTest extends AbstractTest {
+
 
     @Test
     void should_rearrange_game_when_pit_one_moved() {
         Board board = kalahService.newGame();
-        ruleService.replace(board,1);
-        Assertions.assertEquals(0,board.getPitsMap().get(1));
-        Assertions.assertEquals(7,board.getPitsMap().get(2));
-        Assertions.assertEquals(1,board.getPitsMap().get(7));
-        Assertions.assertEquals(6,board.getPitsMap().get(8));
+        ruleService.replace(board, 1);
+        Assertions.assertEquals(0, board.getPitsMap().get(1));
+        Assertions.assertEquals(7, board.getPitsMap().get(2));
+        Assertions.assertEquals(1, board.getPitsMap().get(7));
+        Assertions.assertEquals(6, board.getPitsMap().get(8));
         boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_kalah() {
         Board board = kalahService.newGame();
-        Assertions.assertThrows(ServiceException.class,()-> ruleService.validate(Optional.of( board),1,0));
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 0));
         boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_game_not_found() {
-        Board board = kalahService.newGame();
-        Assertions.assertThrows(ServiceException.class,()-> ruleService.validate(Optional.of( board),2,1));
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.empty(), 2000, 1));
         boardRepository.deleteAll();
     }
 
@@ -41,23 +39,23 @@ class MoveRuleServiceImplTest extends AbstractTest {
     void should_throw_exception_when_game_is_over() {
         Board board = kalahService.newGame();
         board.setIsOver(true);
-        Assertions.assertThrows(ServiceException.class,()-> ruleService.validate(Optional.of( board),1,1));
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 1));
         boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_not_yours() {
         Board board = kalahService.newGame();
-        Assertions.assertThrows(ServiceException.class,()-> ruleService.validate(Optional.of( board),1,8));
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 8));
         boardRepository.deleteAll();
     }
 
     @Test
     void should_throw_exception_when_pit_is_empty() {
         Board board = kalahService.newGame();
-        ruleService.replace(board,1);
-        Assertions.assertEquals(0,board.getPitsMap().get(1));
-        Assertions.assertThrows(ServiceException.class,()-> ruleService.validate( Optional.of( board),1,1));
+        ruleService.replace(board, 1);
+        Assertions.assertEquals(0, board.getPitsMap().get(1));
+        Assertions.assertThrows(ServiceException.class, () -> ruleService.validate(Optional.of(board), 1, 1));
         boardRepository.deleteAll();
     }
 
